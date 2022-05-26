a11f43fdcc9f75c98e9c48bfe7816db23a46ccfd
jbradley84 <utbradley84@yahoo.com>
2022-05-25 20:18:38 -0500

    added array of questions and answers quizArray

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// document id types: quiz-box, header, timer, quiz-card, quiz-question, button
+
+// array of quiz objects - question, answers, correct answer
+let quizArray = [
+    {
+        question: "How much is that doggy in the _________?",
+        answer: ["1. cage", "2. window", "3. dog house", "4. backyard"]
+    },
+    {
+        question: "Now I know my ABCs. Next time won't you _________ with me?",
+        answer: ["1. rap", "2. play", "3. sing", "4. learn"]
+    },
+    {
+        question: "Supercalifragilisticexpiali_________.",
+        answer: ["1. trocious", "2. bocious", "3. growsis", "4. docious"]
+    },
+    {
+        question: "The itsy-bitsy __________ went up the water spout.",
+        answer: ["1. spider", "2. mouse", "3. caterpillar", "4. goldfish"]
+    },
+    {
+        question: "Never gonna _________ you up. Never gonna let you down.",
+        answer: ["1. lift", "2. give", "3. mess", "4. eat"]
+    },
+];
+console.log(quizArray);
