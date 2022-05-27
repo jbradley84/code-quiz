dbb5f9760469fec605af65635bc7fa819649d09b
jbradley84 <utbradley84@yahoo.com>
2022-05-27 16:02:23 -0500

    added quiz timer functionality in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,52 +5,43 @@ var quizInstructions = document.querySelector("#instructions")
 var quizButtons = document.querySelector("#button")
 var buttonsDiv = document.querySelector("#buttons-div")
 
+
 // array of quiz objects - question, answers, correct answer
 let quizArray = [
     {
         question: "How much is that doggy in the _________?",
         answer: ["1. cage", "2. window", "3. dog house", "4. backyard"],
-        correct: 2
+        correct: [1]
     },
     {
         question: "Now I know my ABCs. Next time won't you _________ with me?",
         answer: ["1. rap", "2. play", "3. sing", "4. learn"],
-        correct: 3
+        correct: [2]
     },
     {
         question: "Supercalifragilisticexpiali_________.",
         answer: ["1. trocious", "2. bocious", "3. growsis", "4. docious"],
-        correct: 4
+        correct: [3]
     },
     {
         question: "The itsy-bitsy __________ went up the water spout.",
         answer: ["1. spider", "2. mouse", "3. caterpillar", "4. goldfish"],
-        correct: 1
+        correct: [0]
     },
     {
         question: "Never gonna _________ you up. Never gonna let you down.",
         answer: ["1. lift", "2. give", "3. mess", "4. eat"],
-        correct: 2
+        correct: [1]
     },
 ];
 
+
 // function to start quiz
- let startQuiz = function() {
-     //quizCards.remove();
-     changeQuizCards();
-    // append quizCards with quizArray[0].question
-
-    // append quizCards with quizArray[0].answer
-
-    // append #quiz-button with quizArray.answer[0]
-
-    // start timer
-}
-
-
-// function to change quizCards
-function changeQuizCards() {
+function startQuiz() {
+    // replace "coding quiz challenge" with quiz question
     quizQuestions.textContent = quizArray[0].question;
+
+    // generate quiz buttons
     quizArray[0].answer.forEach(element => {
         var button = document.createElement("button");
         button.id = element;
@@ -59,52 +50,43 @@ function changeQuizCards() {
         button.appendChild(text);
         buttonsDiv.appendChild(button);
     });
+
+    // hide quiz instructions and start button
     quizInstructions.classList.add("hide");
     quizButtons.classList.add("hide");
+
+    // start timer
+    quizTimer();
 }
+
+
+// function to start timer
+function quizTimer() {
+    // set start time to 75 seconds
+    let timerStart = 5;
+    // decrement timer by 1 second
+    let countDown = setInterval(function () {
+        document.getElementById("timer").innerHTML = "Time: " + timerStart;
+        timerStart--;
+    }, 1000);
+    // if timer reaches 0, #timer = "Time: 0"
+    if (countDown <= 0) {
+        clearInterval(countDown);
+    }
+}    
+
 
 // function to increment quizArray question and buttons
 function nextQuestion() {
+
     console.log("quiz button click");
-}
+};
 
-
-
-// function insertQuestions() {
-//     quizQuestions.textContent = quizArray[0].question;
-//     console.log(quizArray[0].answer);
-    // quizButtons.document.createElement("button");
-    // buttons.type = "button";
-    // function buttonOptions(arr) {
-    //     for (var i = 0; i < arr.length; i++) {
-    //         document.querySelector("#button").append("<button value=" + arr[i] + ">" + arr[i] + "</button>");
-    //     }
-    // }
-    // buttonOptions(quizArray[0].answer);
-    
-    //
-    //for (var i = 0; i < quizArray.length; i++) {
-        
-
-        // var answerButtons = document.createElement("button");
-        // button.type = "button";
-        // button.innerHTML = quizArray[0].answer;
-        // button.className = "button"
-        //answerButtons.textContent = quizArray.answer[i];
-        //answerButtons.setAttribute("value", quizArray[i]);
-        //quizQuestions.appendChild(answerButtons);
-        //console.log(answerButtons);
-    //}
-//}
 
 // START QUIZ button event listener
 document.getElementById("button").addEventListener("click", startQuiz);
 
 // QUIZ BUTTON event listener
 document.getElementById("buttons-div").addEventListener("click", nextQuestion);
-
-
-
-
 
 
