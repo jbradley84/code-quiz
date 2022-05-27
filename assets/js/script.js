539b74d6b1ea335e6869511f192c46bcf2cd4c69
jbradley84 <utbradley84@yahoo.com>
2022-05-26 19:15:34 -0500

    started for loop for question array

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,36 +7,61 @@ var quizButtons = document.querySelector("#button")
 let quizArray = [
     {
         question: "How much is that doggy in the _________?",
-        answer: ["1. cage", "2. window", "3. dog house", "4. backyard"]
+        answer: {1: "1. cage", 2: "2. window", 3: "3. dog house", 4: "4. backyard"},
+        correct: 2
     },
     {
         question: "Now I know my ABCs. Next time won't you _________ with me?",
-        answer: ["1. rap", "2. play", "3. sing", "4. learn"]
+        answer: {1: "1. rap", 2: "2. play", 3: "3. sing", 4: "4. learn"},
+        correct: 3
     },
     {
         question: "Supercalifragilisticexpiali_________.",
-        answer: ["1. trocious", "2. bocious", "3. growsis", "4. docious"]
+        answer: {1: "1. trocious", 2: "2. bocious", 3: "3. growsis", 4: "4. docious"},
+        correct: 4
     },
     {
         question: "The itsy-bitsy __________ went up the water spout.",
-        answer: ["1. spider", "2. mouse", "3. caterpillar", "4. goldfish"]
+        answer: {1: "1. spider", 2: "2. mouse", 3: "3. caterpillar", 4: "4. goldfish"},
+        correct: 1
     },
     {
         question: "Never gonna _________ you up. Never gonna let you down.",
-        answer: ["1. lift", "2. give", "3. mess", "4. eat"]
+        answer: {1: "1. lift", 2: "2. give", 3: "3. mess", 4: "4. eat"},
+        correct: 2
     },
 ];
-console.log(quizArray);
 
 // function to start quiz
-let startQuiz = function() {
-    quizCards.remove()    
+ let startQuiz = function() {
+     //quizCards.remove();
+     insertQuestions();
+    // append quizCards with quizArray[0].question
+
+    // append quizCards with quizArray[0].answer
+
+    // append #quiz-button with quizArray.answer[0]
+
+    // start timer
 }
 
 
+
+
+
+function insertQuestions() {
+    quizQuestions.textContent = quizArray[0].question;
+    for (var i = 0; i < quizArray.length; i++) {
+        var answerButtons = document.createElement("button");
+        answerButtons.textContent = quizArray.answer[i];
+        answerButtons.setAttribute("value", quizArray[i]);
+        quizQuestions.appendChild(answerButtons);
+        console.log(answerButtons);
+    }
+}
+
 // start quiz button event listener
 document.getElementById("button").addEventListener("click", startQuiz);
-
 
 
 
