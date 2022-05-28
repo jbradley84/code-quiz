9afabf59eb9d7c3d6c1a78f5ec1127d8d535b98a
jbradley84 <utbradley84@yahoo.com>
2022-05-28 11:39:18 -0500

    added true/false data attributes to buttons based on quiz answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 // document id types: quiz-box, header, timer, quiz-card, quiz-question, button
+var quizHeader = document.querySelector("#header")
 var quizGreeting = document.querySelector("#quiz-greeting")
 var quizInstructions = document.querySelector("#instructions")
 var quizStartBtn = document.querySelector("#quiz-start-btn")
-var quizCards = document.querySelector("#quiz-card")
 var quizQuestions = document.querySelector("#questions-h2")
 var quizButtons = document.querySelector("#button")
 var buttonsDiv = document.querySelector("#buttons-div")
+var quizCards = document.querySelector("#quiz-cards")
+var quizFinished = document.querySelector("#results-h2")
+var quizScore = document.querySelector("#final-score")
+var quizInitials = document.querySelector("#initials-input")
+var highScores = document.querySelector("#leaderboard")
+var mvpList = document.querySelector("mvp-list")
+var goBackBtn = document.querySelector("#go-back")
 var index = 0;
 
 // array of quiz objects - question, answers, correct answer
@@ -28,49 +35,22 @@ let quizArray = [
     {
         question: "The itsy-bitsy __________ went up the water spout.",
         answer: ["1. spider", "2. mouse", "3. caterpillar", "4. goldfish"],
-        correct: "spider"
+        correct: "1. spider"
     },
     {
         question: "Never gonna _________ you up. Never gonna let you down.",
         answer: ["1. lift", "2. give", "3. mess", "4. eat"],
-        correct: "give"
+        correct: "2. give"
     },
 ];
-
-// assign initial value to hidden quiz question
-quizQuestions.textContent = quizArray[index].question;
-
-// assign initial values to hidden quiz buttons
-one.textContent = quizArray[index].answer[0];
-// if (quizArray[index].answer[0] == quizArray[index].correct) {
-//     one.setAttribute("data-answer-value", "true");
-// } else {
-//     one.setAttribute("data-answer-value", "false");
-// }
-two.textContent = quizArray[index].answer[1];
-// if (quizArray[index].answer[0] == quizArray[index].correct) {
-//     two.setAttribute("data-answer-value", "true");
-// } else {
-//     two.setAttribute("data-answer-value", "false");
-// }
-three.textContent = quizArray[index].answer[2];
-// if (quizArray[index].answer[0] == quizArray[index].correct) {
-//     three.setAttribute("data-answer-value", "true");
-// } else {
-//     three.setAttribute("data-answer-value", "false");
-// }
-four.textContent = quizArray[index].answer[3];
-// if (quizArray[index].answer[0] == quizArray[index].correct) {
-//     four.setAttribute("data-answer-value", "true");
-// } else {
-//     four.setAttribute("data-answer-value", "false");
-// }
 
 
 // function to start quiz
 function startQuiz() {
     // hide quiz greeting, instructions, and start button
     quizGreeting.classList.add("hide");
+    quizInstructions.classList.add("hide");
+    quizStartBtn.classList.add("hide");
 
     // reveal quiz question
     quizQuestions.classList.add("reveal");
@@ -81,7 +61,10 @@ function startQuiz() {
     // start timer
     quizTimer();
 
+    // proceed to next question when answering previous question
     nextQuestion();
+
+
 }
 
 
@@ -89,9 +72,39 @@ function startQuiz() {
 function nextQuestion() {
     quizQuestions.textContent = quizArray[index].question;
     one.textContent = quizArray[index].answer[0];
+    console.log(quizArray[index].answer[0]);
+    console.log(quizArray[index].correct);
+    if (one.textContent == quizArray[index].correct) {
+        one.setAttribute("data-answer-value", "true");
+    } else {
+        one.setAttribute("data-answer-value", "false");
+    }
     two.textContent = quizArray[index].answer[1];
+    console.log(quizArray[index].answer[1]);
+    console.log(quizArray[index].correct);
+    if (two.textContent == quizArray[index].correct) {
+        two.setAttribute("data-answer-value", "true");
+    } else {
+        two.setAttribute("data-answer-value", "false");
+    }
     three.textContent = quizArray[index].answer[2];
+    console.log(quizArray[index].answer[2]);
+    console.log(quizArray[index].correct);
+    if (three.textContent == quizArray[index].correct) {
+        three.setAttribute("data-answer-value", "true");
+    } else {
+        three.setAttribute("data-answer-value", "false");
+    }
     four.textContent = quizArray[index].answer[3];
+    console.log(quizArray[index].answer[3]);
+    console.log(quizArray[index].correct);
+    if (four.textContent == quizArray[index].correct) {
+        four.setAttribute("data-answer-value", "true");
+    } else {
+        four.setAttribute("data-answer-value", "false");
+    }
+
+
     // if (quizArray[index].answer[index] === quizArray[index].correct) {
     //     console.log("correct");
     //     // } else {
@@ -99,8 +112,12 @@ function nextQuestion() {
     // }
     if (index < quizArray.length) {
         index++;
+    } else if (index = quizArray.length) {
+        displayResults();
     }
+
 }
+
 
 
 // function to start timer
@@ -119,13 +136,70 @@ function quizTimer() {
 }
 
 
-// START QUIZ button event listener
+// function to display quiz results quiz-h2 final-score score-input
+function displayResults() {
+    // hide quiz questions, and quiz buttons
+    quizCards.classList.add("hide");
+
+    // reveal quiz score results and form to input/record player initials
+    quizFinished.classList.add("reveal");
+    quizScore.classList.add("reveal");
+    quizInitials.classList.add("reveal");
+
+}
+
+
+// function to view high scores
+function viewHighScores() {
+    // hide quiz greeting, instructions, and start button
+    quizHeader.classList.add("hide");
+    quizGreeting.classList.add("hide");
+    quizInstructions.classList.add("hide");
+    quizStartBtn.classList.add("hide");
+
+    // hide quiz questions, and quiz buttons
+    quizQuestions.classList.add("hide");
+    buttonsDiv.classList.add("hide");
+
+    // reveal high scores card
+    highScores.classList.add("reveal");
+    mvpList.classList.add("reveal");
+    goBackBtn.classList.add("reveal");
+}
+
+
+// function to return to quiz greeting when go back button is clicked
+function quizGreeting() {
+    // hide high scores card
+    quizFinished.classList.add("hide");
+    quizScore.classList.add("hide");
+    quizInitials.classList.add("hide");
+
+    // reveal quiz greetings
+    quizGreeting.classList.add("reveal");
+    quizInstructions.classList.add("reveal");
+    quizStartBtn.classList.add("reveal");
+}
+
+
+// VIEW HIGH SCORES header event listener
+//document.getElementById("show-me-scores").addEventListener("click", viewHighScores);
+
+// GO BACK BUTTON event listener
+//document.getElementById("go-back").addEventListener("click", quizGreeting);
+
+// START QUIZ BUTTON event listener
 document.getElementById("quiz-start-btn").addEventListener("click", startQuiz);
 
-// QUIZ BUTTON event listener
+// ANSWER BUTTONS event listener
 document.getElementById("one").addEventListener("click", nextQuestion);
 document.getElementById("two").addEventListener("click", nextQuestion);
 document.getElementById("three").addEventListener("click", nextQuestion);
 document.getElementById("four").addEventListener("click", nextQuestion);
+
+// ENTER INITIALS SUBMIT BUTTON event listener
+document.getElementById("initials-submit").addEventListener("click", viewHighScores);
+
+
 
 
